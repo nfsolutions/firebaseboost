bfc7426d7ecc4f0c669cb0d8337f7693fb96588f
nydi <stefan.nydegger@gmail.com>
2018-03-08 22:04:47 +0100

    initial version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
-
+import { AngularFireModule } from 'angularfire2';
+import { FirebaseConfig } from '../environments/firebase.config';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(FirebaseConfig.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,10 @@
+export const FirebaseConfig = {
+  firebase: {
+    apiKey: 'AIzaSyCUEeQtlt6maGvhPVn_MrGk1JNOVE4qc78',
+    authDomain: 'fir-demo-ce933.firebaseapp.com',
+    databaseURL: 'https://fir-demo-ce933.firebaseio.com',
+    projectId: 'fir-demo-ce933',
+    storageBucket: 'fir-demo-ce933.appspot.com',
+    messagingSenderId: '915720527781'
+  }
+};
